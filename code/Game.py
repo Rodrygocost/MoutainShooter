b9547bd267479eb38bc2e8d1d2b1e7bc85440872
Rodrygo Andrade <rodrigoandrade311@gmail.com>
2025-08-19 06:52:59 -0300

    Create BG movement

--- code/Game.py
+++ code/Game.py
@@ -1,4 +1,8 @@
 import pygame
+
+
+from code.Const import MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 
 
@@ -10,8 +14,16 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit() # Close
+                quit() #End
+            else:
+             pass
 
             # Check for all events
             # for event in pygame.event.get():
